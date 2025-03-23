f1f1fd1fa3e6be7d6e0e3af2839accb27aeb80f9
Max U <maxu@Maxs-MBP.lan>
2025-03-22 22:46:50 -0500

    initial commit of xcode repo

---
+++ Epic Shots/Epic_ShotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Epic_ShotsApp.swift
+//  Epic Shots
+//
+//  Created by Max U on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Epic_ShotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
